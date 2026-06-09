bb9908d04e426bcc69ff6f3d0ec94fa04db8622f
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-06-09 10:41:49 -0700

    Set basics for tests

--- src/checkout/checkout.controller.spec.ts
+++ src/checkout/checkout.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { CheckoutController } from './checkout.controller';
 import { CheckoutService } from './checkout.service';
+import { Sale } from './entities/sale.entity';
 
 describe('CheckoutController', () => {
   let controller: CheckoutController;
@@ -8,7 +11,17 @@ describe('CheckoutController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CheckoutController],
-      providers: [CheckoutService],
+      providers: [
+        CheckoutService,
+        {
+          provide: getRepositoryToken(Sale),
+          useValue: {},
+        },
+        {
+          provide: DataSource,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<CheckoutController>(CheckoutController);
--- src/checkout/checkout.service.spec.ts
+++ src/checkout/checkout.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { CheckoutService } from './checkout.service';
+import { Sale } from './entities/sale.entity';
 
 describe('CheckoutService', () => {
   let service: CheckoutService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CheckoutService],
+      providers: [
+        CheckoutService,
+        {
+          provide: getRepositoryToken(Sale),
+          useValue: {},
+        },
+        {
+          provide: DataSource,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<CheckoutService>(CheckoutService);
