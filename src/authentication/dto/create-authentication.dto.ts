91250e349a4dc494123699f1e5ac78203d70fbc1
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-06-08 18:55:42 -0700

    Document API Operations examples

--- src/authentication/dto/create-authentication.dto.ts
+++ src/authentication/dto/create-authentication.dto.ts
@@ -5,22 +5,27 @@ import {
   MinLength,
   IsEnum,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '@models/user.models';
 export class CreateAuthenticationDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({example: 'John'})
   name!: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({example: "john@email.com"})
   email!: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @ApiProperty({example: "secret123!"})
   password!: string;
 
   @IsEnum(UserRole)
   @IsNotEmpty()
+  @ApiProperty({enum: UserRole, example: UserRole.Admin})
   role!: UserRole;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,22 +6,26 @@ import {
   MinLength,
 } from 'class-validator';
 import { UserRole } from '@models/user.models';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'John' })
   name!: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({ example: 'john@email.com' })
   email!: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @ApiProperty({ example: 'secret123!' })
   password!: string;
 
   @IsEnum(UserRole)
   @IsNotEmpty()
+  @ApiProperty({ enum: UserRole, example: UserRole.Admin })
   role!: UserRole;
 }
